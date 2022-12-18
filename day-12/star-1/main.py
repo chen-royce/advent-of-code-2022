b5cddda50afd38df6efd03f31b9eee10314565c5
Royce Chen <royce.chen.y@gmail.com>
2022-12-17 19:53:09 -0500

    Refactor, refactor, save progress

--- day-12/star-1/main.py
+++ day-12/star-1/main.py
@@ -1,24 +1,24 @@
-def open_file(filepath):
-    file = open(filepath, "r")
-    return file
-
-
-def read_input(file):
-
+def parse(file):
     rows = []
-
     for row in file.readlines():
         toAdd = []
         for char in row:
             if char != '\n':
                 toAdd += char
         rows.append(toAdd)
-
     return rows
+
+def find_start(input):
+    for i in range(len(input)):
+        for j in range(len(input[0])):
+
 
 
 def main():
-    read_input(open_file("./problem/sample.txt"))
+    filepath = "./problem/sample.txt"
+    file = open(filepath, "r")
+    input = parse(file)
+    my_list = [1,2,3]
 
 
 if __name__ == "__main__":
