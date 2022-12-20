1eba0d3ee23acd53b1f549af573e325516621ccb
Royce Chen <94400272+chen-royce@users.noreply.github.com>
2022-12-19 19:51:56 -0500

    Fix converter \(^o^)/

--- day-13/star-1/converter.py
+++ day-13/star-1/converter.py
@@ -1,48 +1,63 @@
-LIST_TYPE = type([])
-INT_TYPE = type(1)
+STRING_TYPE = type("")
 
+def find_closing_bracket(text):
+    l_brackets = 0
+    r_brackets = 0
+    for i in range(len(text)):
+        if text[i] == "[":
+            l_brackets += 1
+        if text[i] == "]":
+            r_brackets += 1
+            if l_brackets == r_brackets:
+                return i
+    raise Exception("can't find closing bracket")
 
-# def convert_list_items(text):
-#     return text.split(",")
+def divide_into_values(text):
+    ret = []
+    while text.find("[") != -1:
+        # find bracketed area
+        list_start = text.find("[")
+        list_end = find_closing_bracket(text)
+
+        # if there's stuff before the bracket, add it first
+        if list_start != 0:
+            head = text[:list_start-1].split(",") # 1 to omit comma
+            for h in head:
+                ret.append(int(h))
+        
+        # otherwise, just add the bracket
+        ret.append(text[list_start:list_end+1])
+
+        # trim string and repeat
+        text = text[list_end+2:]
+
+    # if rest of string is not empty, append it
+    if len(text) > 0:
+        split = text.split(",")
+        for item in split:
+            ret.append(int(item))
+    
+    return ret
+
 
 
 def convert_input(text):
-    print("CALLING CONVERT INPUT ON:")
-    print(text)
     # base case: no brackets to be found
     if "[" not in text:
-        strings = text.split(",")
-        if len(strings) > 1:
-            ints = []
-            for str in strings:
-                ints.append(int(str))
-            return ints
-        else:
-            return int(strings[0])
-
+        return int(text)
+    
     # if brackets found
     list_start = text.index("[")
-    list_end = (len(text) - 1) - text[::-1].index("]")
+    list_end = find_closing_bracket(text)
 
-    if list_start == 0 and list_end == len(text) - 1:
-        print("HERE 0")
-        return convert_input(text[list_start+1:list_end])
+    text = text[list_start+1:list_end]
+    items = divide_into_values(text)
 
     ret = []
 
-    # example: input is 0,[1,2,3]
-    # we want to handle just 0
-    if list_start != 0:
-        print("HERE 1")
-        ret.append(convert_input(text[0:list_start-1]))
-
-    print("HERE 2")
-    ret.append(convert_input(text[list_start+1:list_end]))
-
-    # example: input is [1,2,3],0
-    # we want to handle just 0
-    if list_end != len(text)-1:
-        print("HERE 3")
-        ret.append(convert_input(text[list_end+2:]))
+    for item in items:
+        if type(item) == STRING_TYPE:
+            item = convert_input(item)
+        ret.append(item)
 
     return ret
